e81569379e0b3eba34fa1adbf5d407c5237b8752
komerajaya456 <komerajaya7259@gmail.com>
2023-10-15 16:57:28 +0530

    exporing using inbuilt by defalut ES module(unlike module.expoerts,exports),by using [.mjs]extension (export default add,sub)(import jk from ./file.mjs) two files should be in .mjs format

---
+++ Node js/nodejspractice/imp,exp/exp4.mjs
@@ -0,0 +1,8 @@
+const multi=(a,b)=>{
+  return a*b
+}
+
+const divi=(a,b)=>{
+  return a/b
+}
+export default {multi,divi}
---
+++ Node js/nodejspractice/imp,exp/imp4.mjs
@@ -0,0 +1,3 @@
+import jk from "./exp4.mjs"
+console.log(jk.multi(3,3))
+console.log(jk.divi(3,3))
