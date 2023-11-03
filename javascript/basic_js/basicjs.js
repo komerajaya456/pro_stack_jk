0554717116dba11b64ef0034f1816a40edf56ee7
komerajaya456 <komerajaya7259@gmail.com>
2023-11-03 12:54:18 +0530

    con.log,con.warn,con.error,con.clear() ,we can click button by giving one DOM function lik ,dom.getele...ById().click()

---
+++ javascript/basic_js/basicjs.js
@@ -0,0 +1,18 @@
+console.log(typeof 8)
+console.log(typeof 'hi')
+console.log(typeof (5==5))
+
+
+console.warn('this is warning methon in console object')
+console.error('this is error method in console object')
+console.clear()// clear all above consoles lines not below consoles
+console.error('this is error method in console object')
+
+//click the button by running js
+//we can click button at any time we want
+//document.querySelector('#btn').click()
+function kom(){
+  const x=document.getElementById("para")
+  x.innerHTML="changed"
+}
+
--- javascript/basicjs.js
+++
@@ -1,4 +0,0 @@
-console.log(typeof 8)
-console.log(typeof 'hi')
-console.log(typeof (5==5))
-
