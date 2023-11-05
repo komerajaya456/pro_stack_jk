0dad1e48bf2899359355d84507c8634af63711e6
komerajaya456 <komerajaya7259@gmail.com>
2023-11-05 21:17:43 +0530

    learnt variabke const,let,var accesablity && if else in one-line condition

--- javascript/basic_js/basicjs.js
+++ javascript/basic_js/basicjs.js
@@ -1,3 +1,4 @@
+
 console.log(typeof 8)
 console.log(typeof 'hi')
 console.log(typeof (5==5))
@@ -16,3 +17,23 @@ function kom(){
   x.innerHTML="changed"
 }
 
+console.log(Math.random()*3)
+
+console.clear()
+
+const age=19;
+if (age>=18 && age<=65){
+  var age1=55;
+  console.log(age) 
+  console.log("age bet 18 & 65")
+}
+else if(age>100){
+  console.log("greter 100")
+}
+console.log(age1)
+function hello(){
+  let x=4;
+  console.log(age)
+}
+hello()
+console.log(x)
--- javascript/if-else/indexif.js
+++ javascript/if-else/indexif.js
@@ -1,3 +1,8 @@
 let age=14
 if (age>18) console.log("u can drive")
 else console.log("u cant drive")
+
+//one line if-else statement
+let age1=18;
+let result=age1>18 ? "u can drive" : "u cant drive1" ;
+console.log(result)
