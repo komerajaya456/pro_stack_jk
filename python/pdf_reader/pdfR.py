cdaa122e987676d9d8094f911fa9e8f6a9ba788f
komerajaya456 <komerajaya7259@gmail.com>
2024-02-16 13:42:54 +0530

    added 1pdf

--- python/pdf_reader/pdfR.py
+++ python/pdf_reader/pdfR.py
@@ -3,17 +3,11 @@ import fitz  # PyMuPDF
 def extract_text_with_ocr(pdf_path):
     text = ""
     doc = fitz.open(pdf_path)
-
-    i=0
     for page_num in range(doc.page_count):
-
-        i=i+1
-
         page = doc.load_page(page_num)
         text += page.get_text()
-        print(i,'kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk')
     return text
 
-pdf_path = "sample.pdf"
+pdf_path = "Komera Jaya Krishna resume.pdf"
 extracted_text = extract_text_with_ocr(pdf_path)
 print(extracted_text)
